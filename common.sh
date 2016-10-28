0fa681f7b8282644287dab9bcc85b955787aabe7
shogo muranushi <muranushi@cloudpack.jp>
2016-10-28 10:15:28 +0900

    add *.sh

--- common.sh
+++ common.sh
@@ -19,19 +19,6 @@ sshd: ${sshd}
 EOF
 
 ##########################
-## Wordpress Parameter
-##########################
-cat <<EOF > c-ansible/roles/wordpress/vars/main.yml
-wp_db_name: ${db_name}
-wp_db_user: ${db_user}
-wp_db_password: ${db_password}
-wp_dir: /var/www/html
-mysql_host: ${db_host}
-mysql_port: ${db_port}
-auto_up_disable: false
-core_update_level: true
-EOF
-##########################
 ## Ansible Exec
 ##########################
-/usr/local/bin/ansible-playbook c-ansible/apache24+php56+wordpress.yml
+/usr/local/bin/ansible-playbook c-ansible/common.yml
